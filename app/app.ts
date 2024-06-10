d9b0ea3868487db561c2fc52165bdc77b7e43172
WorkingTheo <120393948+WorkingTheo@users.noreply.github.com>
2024-06-10 12:40:27 +0100

    Check your answers (#2)
    * feat(cya): add a check-your-answers page
    
    * feat(cya link): add link to cya from start page
    
    * feat(cya link ): rename it to 'check your answers'
    
    * feat(backlink ): add a semi-working backlink
    
    ---------
    
    Co-authored-by: theo.selenskiy <theo.selenskiy@engineering.digital.dwp.gov.uk>

--- app/app.ts
+++ app/app.ts
@@ -6,6 +6,7 @@ import express, { Request, Response } from 'express';
 import { pages } from './pages';
 import { plan } from './plan';
 import axios from 'axios';
+import { checkYourAnswers } from './check-your-answers';
 
 export const FOUND_ADDRESSES_DATA = 'found-addresses-data';
 
@@ -15,6 +16,10 @@ function getDataForPage(req: Request, waypoint: string) {
 
 function setDataForPage(req: Request, waypoint: string, data: any) {
   (req as any).casa.journeyContext.setDataForPage(waypoint, data);
+}
+
+function setBacklink(req: Request, res: Response) {
+  res.locals.casa.journeyPreviousUrl = req.session.previousUrl;
 }
 
 export const removeWaypointsFromJourneyContext = (req: Request, waypoints: string[], includeTempData?: boolean) => {
@@ -61,16 +66,26 @@ const app = (
   });
 
   ancillaryRouter.use('/start', (req: Request, res: Response) => {
-    removeWaypointsFromJourneyContext(req, ['temp-address-confirmation']);
+    removeWaypointsFromJourneyContext(req, ['temp-address-confirmation', 'edit']);
 
     JourneyContext.putContext(req.session, (req as any).casa.journeyContext);
     req.session.save(() => res.render('pages/start.njk'));
   });
 
+  ancillaryRouter.use('/check-your-answers', (req: Request, res: Response) => {
+    checkYourAnswers(req, res);
+    req.session.previousUrl = req.originalUrl;
+    res.render('pages/check-your-answers.njk');
+  });
+
+  ancillaryRouter.use('/edit-address', (req: Request, res: Response) => {
+    setDataForPage(req, 'edit', { edit: true });
+    res.redirect('/address-confirmation');
+  });
+
   ancillaryRouter.use('/address-route', (req: Request, res: Response) => {
     const addressConfirmationData = getDataForPage(req, 'address-confirmation') as { address: string };
-    console.log('address-route');
-    console.log({ data: addressConfirmationData });
+
     if (addressConfirmationData?.address !== undefined) {
       res.redirect('/address-confirmation');
       return;
@@ -89,17 +104,17 @@ const app = (
   ancillaryRouter.use('/post-code', async (req: Request, res: Response) => {
     if (req.method === 'GET') {
       res.locals.formData = getDataForPage(req, 'post-code');
+      setBacklink(req, res);
       res.render('pages/post-code.njk');
     }
 
     if (req.method === 'POST') {
-      //handle data consumption;
-      console.log({ body: req.body });
       const data = req.body;
       const results = await axios.post<string[]>('http://localhost:3001/address', data);
       const addresses = results.data;
       setDataForPage(req, FOUND_ADDRESSES_DATA, { addresses });
       setDataForPage(req, 'post-code', { postcode: req.body.postcode });
+      req.session.previousUrl = req.originalUrl;
       res.redirect('/post-code-results');
     }
   });
@@ -108,6 +123,7 @@ const app = (
     if (req.method === 'GET') {
       const { addresses } = getDataForPage(req, FOUND_ADDRESSES_DATA) as { addresses: string[] };
       const addressOptions = addresses.map(address => ({ value: address, text: address }));
+      setBacklink(req, res);
       res.locals.addressOptions = addressOptions;
       res.locals.formData = getDataForPage(req, 'post-code-results');
       res.render('pages/post-code-results.njk');
@@ -116,6 +132,7 @@ const app = (
     if (req.method === 'POST') {
       setDataForPage(req, 'post-code-results', { address: req.body.address });
       setDataForPage(req, 'temp-address-confirmation', { address: req.body.address });
+      req.session.previousUrl = req.originalUrl;
       res.redirect('/address-confirmation');
     }
   });
@@ -124,6 +141,7 @@ const app = (
     const tempData = getDataForPage(req, 'temp-address-confirmation');
     if (req.method === 'GET') {
       const data = getDataForPage(req, 'address-confirmation');
+      setBacklink(req, res);
       res.locals.formData = tempData ?? data;
       res.render('pages/address-confirmation');
     }
@@ -131,20 +149,30 @@ const app = (
     if (req.method === 'POST') {
       setDataForPage(req, 'address-confirmation', tempData);
       removeWaypointsFromJourneyContext(req, ['temp-address-confirmation']);
+      const edit = getDataForPage(req, 'edit');
 
+      req.session.previousUrl = req.originalUrl;
       JourneyContext.putContext(req.session, (req as any).casa.journeyContext);
-      req.session.save(() => res.redirect('/start'));
+      req.session.save(() => {
+        if(edit?.edit) {
+          res.redirect('/check-your-answers')
+        } else {
+          res.redirect('/start');
+        }
+      });
     }
   });
 
   ancillaryRouter.use('/address-manual', (req: Request, res: Response) => {
     if (req.method === 'GET') {
+      setBacklink(req, res);
       res.render('pages/address-manual.njk');
     }
 
     if (req.method === 'POST') {
       const { addressLine1, addressLine2, town, county, postCode } = req.body;
       setDataForPage(req, 'temp-address-confirmation', { address: `${addressLine1} - ${postCode}` });
+      req.session.previousUrl = req.originalUrl;
       res.redirect('/address-confirmation');
     }
   })
---
+++ app/check-your-answers.ts
@@ -0,0 +1,75 @@
+import { JourneyContext, waypointUrl } from "@dwp/govuk-casa";
+import { Request, Response } from "express";
+import { removeWaypointsFromJourneyContext } from './app';
+
+export function patientDetailsSection(journeyContext: JourneyContext, res: Response) {
+  const nameData = journeyContext.getDataForPage('name') as { name: string };
+  const nameRow = {
+    key: {
+      text: 'Patient Name'
+    },
+    value: {
+      text: nameData.name,
+    },
+    actions: {
+      items: [
+        {
+          text: "Change your answer",
+          href: waypointUrl({ waypoint: 'name', edit: true, editOrigin: 'check-your-answers' })
+        }
+      ]
+    }
+  };
+
+  const surnameData = journeyContext.getDataForPage('surname') as { surname: string };
+  const surnameRow = {
+    key: {
+      text: 'Patient Surname'
+    },
+    value: {
+      text: surnameData.surname,
+    },
+    actions: {
+      items: [
+        {
+          text: "Change your answer",
+          href: waypointUrl({ waypoint: 'surname', edit: true, editOrigin: 'check-your-answers' })
+        }
+      ]
+    }
+  };
+
+  const addressData = journeyContext.getDataForPage('address-confirmation') as { address: string };
+  const addressRow = {
+    key: {
+      text: 'Address'
+    },
+    value: {
+      text: addressData.address,
+    },
+    actions: {
+      items: [
+        {
+          text: "Change your answer",
+          href: "/edit-address"
+        }
+      ]
+    }
+  };
+
+  res.locals.patientDetailsRows = [
+    nameRow,
+    surnameRow,
+    addressRow
+  ];
+}
+export function checkYourAnswers(req: Request, res: Response) {
+  const journeyContext = JourneyContext.getDefaultContext(req.session);
+
+  journeyContext.setDataForPage('edit', { edit: false });
+  removeWaypointsFromJourneyContext(req, ['skippedTo', 'skippedFrom']);
+  patientDetailsSection(journeyContext, res);
+
+  JourneyContext.putContext(req.session, (req as any).casa.journeyContext);
+  req.session.save();
+}
